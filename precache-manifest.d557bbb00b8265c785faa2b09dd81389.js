ade497a078f32cbbd62d093d0349111aca63cb2d
raman pratap <ramanprtap>
2020-05-11 18:58:57 +0530

    Updates

---
+++ precache-manifest.d557bbb00b8265c785faa2b09dd81389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58b29691a21900d444d5407c8aa12e4",
+    "url": "/PWA-APP/index.html"
+  },
+  {
+    "revision": "89e2d19f4d54b1be228a",
+    "url": "/PWA-APP/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea4fdd77cf4f68f01325",
+    "url": "/PWA-APP/static/js/2.fcc91d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PWA-APP/static/js/2.fcc91d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e2d19f4d54b1be228a",
+    "url": "/PWA-APP/static/js/main.437fd12e.chunk.js"
+  },
+  {
+    "revision": "86b2ea2fc4b79e090478",
+    "url": "/PWA-APP/static/js/runtime-main.0275cb80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PWA-APP/static/media/logo.5d5d9eef.svg"
+  }
+]);
